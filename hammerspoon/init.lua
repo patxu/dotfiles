bc2e8d48c1c765eba452868c2ae25d05b9e87e81
patxu <patrick.s.xu.17@dartmouth.edu>
2017-01-15 19:13:26 -0500

    update hammerspoon path

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -219,7 +219,7 @@ function reload_config(files)
 end
 -- hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 
-hs.pathwatcher.new(os.getenv("HOME") .. "/code/workspace_settings/hammerspoon/", reload_config):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "/code/dotfiles/hammerspoon/", reload_config):start()
 hs.alert.show("Config loaded")
 
 -----------------------------------------------
