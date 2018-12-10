8a2759fc921ebe994da5126299142f5d44b3c96c
patxu <patrickxu13@gmail.com>
2018-12-10 13:27:34 -0800

    update ondemand homedir setup

--- atom/ondemand/homedir/setup.sh
+++ atom/ondemand/homedir/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Ondemand doesn't yet support symlinks
 
-ln -s ~/code/dotfiles/ondemand/homedir/.bash_profile ~/.atom/ondemand/homedir/.bash_profile
-ln -s ~/code/dotfiles/ondemand/homedir/.bashrc ~/.atom/ondemand/homedir/.bashrc
-ln -s ~/code/dotfiles/ondemand/homedir/.hgrc ~/.atom/ondemand/homedir/.hgrc
-ln -s ~/code/dotfiles/ondemand/homedir/.hphpd.ini ~/.atom/ondemand/homedir/.hphpd.ini
-ln -s ~/code/dotfiles/ondemand/homedir/.inputrc ~/.atom/ondemand/homedir/.inputrc
+ln -s ~/code/dotfiles/atom/ondemand/homedir/.bash_profile ~/.atom/ondemand/homedir/.bash_profile
+ln -s ~/code/dotfiles/atom/ondemand/homedir/.bashrc ~/.atom/ondemand/homedir/.bashrc
+ln -s ~/code/dotfiles/atom/ondemand/homedir/.hgrc ~/.atom/ondemand/homedir/.hgrc
+ln -s ~/code/dotfiles/atom/ondemand/homedir/.hphpd.ini ~/.atom/ondemand/homedir/.hphpd.ini
+ln -s ~/code/dotfiles/atom/ondemand/homedir/.inputrc ~/.atom/ondemand/homedir/.inputrc
