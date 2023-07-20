10f137a85c5d515b5445543a45769cf6b3c8fd20
patxu <patrickxu13@gmail.com>
2023-07-20 16:13:58 -0400

    add hushlogin https://medium.com/macoclock/how-to-remove-the-last-login-prompt-from-iterm-terminal-on-macos-8d70dea0f2e

--- bash/setup.sh
+++ bash/setup.sh
@@ -4,3 +4,5 @@ ln -s ~/code/dotfiles/bash/login/.bash_profile ~/.bash_profile
 ln -s ~/code/dotfiles/bash/login/.bashrc ~/.bashrc
 ln -s ~/code/dotfiles/bash/login/.bashrc_common ~/.bashrc_common
 ln -s ~/code/dotfiles/bash/.git-prompt.sh ~/.git-prompt.sh
+# https://medium.com/macoclock/how-to-remove-the-last-login-prompt-from-iterm-terminal-on-macos-8d70dea0f2e
+ln -s ~/code/dotfiles/bash/.hushlogin ~/.hushlogin # remove extra terminal output
